7de3b96f7a17036912b62b90379a67eb345d4e4b
Jason Hamilton-Smith <hs.jason@gmail.com>
2018-12-01 18:11:50 -0800

    Update bootstrap config

--- bootstrap_conf.sh
+++ bootstrap_conf.sh
@@ -6,8 +6,8 @@ sudo apt install \
     zsh vim wget curl git gitk tmux htop \
     jq \
     python python-pip python3-pip virtualenvwrapper python3-distutils \
-    scrot unclutter terminator playerctl libsixel-bin spotify-client \
-    helix-cli slack-desktop keepass2 xdotool
+    scrot unclutter terminator libsixel-bin \
+    keepass2 xdotool
 
 # Pip packages
 sudo -H pip install virtualenv virtualenvwrapper
@@ -15,3 +15,35 @@ sudo -H pip install virtualenv virtualenvwrapper
 # Shell configuration
 curl -sL git.io/antibody | sh -s
 # git clone https://github.com/seebi/dircolors-solarized.git .dircolors
+
+#####################
+# Optional Software #
+#####################
+
+# Media stuff
+echo -n "Install media packages? "
+read REPLY
+if [ $REPLY != "${REPLY#[Yy]}" ]
+then
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90 
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update
+sudo apt-get install spotify-client
+
+
+echo "Please download playerctl manually"
+echo "  https://github.com/acrisci/playerctl/releases/latest"
+echo "Press enter to continue"
+read NADA
+
+fi
+
+# Work Stuff (Hothead)
+echo -n "Install work (Hothead) packages? "
+read REPLY
+if [ $REPLY != "${REPLY#[Yy]}" ]
+then
+sudo apt install \
+    helix-cli slack-desktop
+fi
